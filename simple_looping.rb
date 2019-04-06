8d3190710652c55802e685f9fba1ff138d3701bb
Jessica Hong <jessicahong.24@gmail.com>
2019-04-06 16:57:24 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -39,9 +39,6 @@ end
 
 def for_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  counter 1..number_of_times
-  for saying in phrase
-  puts phrase
-end
+  
 end
 
