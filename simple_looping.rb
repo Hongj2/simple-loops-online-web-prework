d2a4e11541eba0915926681d7dbcf32eaaa5e47a
Jessica Hong <jessicahong.24@gmail.com>
2019-04-06 16:58:32 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,8 +24,7 @@ end
 
 def until_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-
-  until counter == number_of_times
+  
 end
 
 def for_iterator(number_of_times)
